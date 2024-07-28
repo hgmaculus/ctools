0322406af96f8af543460bbb2ec91bcf2e554f21
H. Gabriel Máculus <104532543+hgmaculus@users.noreply.github.com>
2024-07-28 09:34:53 -0300

    Add return_function_pointer.c

---
+++ return_function_pointer.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+typedef void (*functype)();
+
+void h1() { printf("H1\n"); }
+void h2() { printf("H2\n"); }
+void h3() { printf("H3\n"); }
+functype funcs[] = { h1, h2, h3 };
+functype f1() { return h1; }
+int main(int argc, char *argv[])
+{
+  void (*selector)() = f1();
+  selector();
+  return 0;
+}
+/*
+  Aunque no es muy sencillo, es lo mas que pude reducir un ejemplo
+  de funciones que devuelven punteros a funciones.
+  Una llamada a la funcion f1() devuelve un puntero a h1.
+  Una llamada a selector() ejecuta h1()
+*/
