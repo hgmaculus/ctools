31d7c1192363e157bd144eb9e177e6d0ecd8b661
H. Gabriel Máculus <104532543+hgmaculus@users.noreply.github.com>
2024-11-28 07:58:01 -0300

    Add filecount.c

---
+++ filecount.c
@@ -0,0 +1,20 @@
+/* filecount by H. Gabriel Máculus <gabrielmaculus@gmail.com>
+	
+*/
+
+#include <stdio.h>
+
+int main(int argc, char *argv[])
+{
+  FILE *f = fopen(argv[1], "r");
+  if(!f) return -1;
+size_t count=0;
+  while(!feof(f)) {
+    fgetc(f);
+    count++;
+  }
+  printf("Count: %lu\n", count);
+  
+  fclose(f);
+  return 0;
+}
