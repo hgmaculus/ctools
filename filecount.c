93ca0b5f772e0ab36a4c7060120d20017c009a3a
H. Gabriel Maculus <gabrielmaculus@gmail.com>
2026-03-19 06:42:10 -0300

    filecount.c: change to function

--- filecount.c
+++ filecount.c
@@ -1,19 +1,21 @@
 /* filecount by H. Gabriel Máculus <gabrielmaculus@gmail.com>
  Print number of bytes in file specified by argument	
 */
-
 #include <stdio.h>
 
-int main(int argc, char *argv[])
-{
-  FILE *f = fopen(argv[1], "r");
+size_t file_count(const char *restrict filename) {
+  FILE *f = fopen(filename, "r");
   if(!f) return -1;
   size_t count=0;
   while(!feof(f)) {
     fgetc(f);
     count++;
   }
-  printf("Count: %lu\n", count);
   fclose(f);
-  return 0;
+  return count;
+}
+int main(int argc, char *argv[])
+{
+  printf("Count %zu\n", file_count(argv[1]));
+  return 0; 
 }
