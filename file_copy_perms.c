dc45a9d3da8f73cec1dd6447f4540b261ae143ef
H. Gabriel Maculus <gabrielmaculus@gmail.com>
2026-07-23 08:17:45 -0300

    add: file_copy_perms.c

---
+++ file_copy_perms.c
@@ -0,0 +1,118 @@
+#include <stdio.h>
+#include <sys/stat.h>
+#include <sys/fcntl.h>
+#include <sys/types.h>
+
+int file_copy_perms(const char *src, const char *dst)
+{
+    struct stat st;
+    if (stat(src, &st) != 0)
+    {
+        perror("src stat failed");
+        return -1;
+    }
+
+    if (chmod(dst, st.st_mode) != 0)
+    {
+        perror("dst chmod failed");
+        return -1;
+    }
+
+    return 0;
+}
+
+int file_create_with_perms(const char *filename, mode_t mode)
+{
+    FILE *f = fopen(filename, "w");
+    if (!f)
+    {
+        perror("fopen failed");
+        return -1;  
+    }
+    fclose(f);
+    chmod(filename, mode);
+    return 0;
+}
+
+int file_copy_perms_fd(int src, int dst)
+{
+    struct stat fst;
+    if (fstat(src, &fst) != 0)
+    {
+        perror("src fstat failed");
+        return -1;
+    }
+
+    if (fchmod(dst, fst.st_mode) != 0)
+    {
+        perror("dst fchmod failed");
+        return -1;
+    }
+
+    return 0;
+}
+
+int file_compare_perms(const char *src, const char *dst)
+{
+    struct stat src_stat, dst_stat;
+    if (stat(src, &src_stat) != 0)
+    {
+        perror("src stat failed");
+        return -1;
+    }
+
+    if (stat(dst, &dst_stat) != 0)
+    {
+        perror("dst stat failed");
+        return -1;
+    }
+
+    if (src_stat.st_mode != dst_stat.st_mode)
+    {
+        return 1;
+    }
+
+    return 0;
+}
+
+int main(int argc, char *argv[])
+{
+    struct stat st;
+    stat("/proc/sys/vm/nr_hugepages", &st);
+
+    printf("stat.st_mode: %o\n", st.st_mode);
+
+    if (argc != 3)
+    {
+        fprintf(stderr, "Usage: %s <src> <dst>\n", argv[0]);
+        return 1;
+    }
+
+    FILE *src = fopen(argv[1], "r");
+    if (!src)
+    {
+        perror("fopen src failed");
+        return 1;
+    }
+
+    FILE *dst = fopen(argv[2], "w");
+    if (!dst)
+    {
+        perror("fopen dst failed");
+        return 1;
+    }
+
+    if (file_copy_perms(argv[1], argv[2]) != 0)
+    {
+        fprintf(stderr, "file_copy_perms failed\n");
+        return 1;
+    }
+
+    if (file_compare_perms(argv[1], argv[2]) != 0)
+    {
+        fprintf(stderr, "file_compare_perms failed\n");
+        return 1;
+    }
+
+    return 0;
+}
--- get_max_free_mem.c
+++ get_max_free_mem.c
@@ -2,9 +2,9 @@
  *
  */
 
- #define _GNU_SOURCE
- #define _XOPEN_SOURCE 700
- #define _FILE_OFFSET_BITS 64
+#define _GNU_SOURCE
+#define _XOPEN_SOURCE 700
+#define _FILE_OFFSET_BITS 64
 
 #include <unistd.h>
 #include <stdlib.h>
@@ -13,61 +13,62 @@
 
 size_t getfreemem() { return sysconf(_SC_AVPHYS_PAGES) * sysconf(_SC_PAGESIZE); }
 
-size_t get_max_free_mem(size_t size, void **ptr)
-{
-    *ptr = malloc(size);
-    while (*ptr == NULL && size > 0)
-    {
-        size = size / 2;
-        *ptr = malloc(size);
-    }
-    return size;
-}
 int main(int argc, char *argv[])
 {
     printf("Page Size: %d\n", getpagesize());
 
     size_t t = 1024 * 1024 * 1024; // 1GB
     void *ptr = NULL;
-    printf("get_max_free_mem: %lu\n", get_max_free_mem(t, &ptr));
-    free(ptr);
-
+    
+    
     ptr = malloc(4ll * 1024 * 1024 * 1024); // 4GB
-    if (ptr == NULL) {
+    if (ptr == NULL)
+    {
         printf("malloc failed for 4GB\n");
         return 1;
-    } else {
+    }
+    else
+    {
         printf("malloc succeeded for 4GB\n");
     }
     free(ptr);
-    
+
     // mmap section with 2MB page size
     ptr = mmap(NULL, 1024ll * 1024 * 1024, PROT_READ | PROT_WRITE, MAP_PRIVATE | MAP_ANONYMOUS | MAP_HUGETLB | MAP_HUGE_2MB, -1, 0); // 1GB
-    if (ptr == MAP_FAILED) {
+    if (ptr == MAP_FAILED)
+    {
         perror("mmap failed for 1GB with 2MB page size");
         return 1;
-    } else {
+    }
+    else
+    {
         printf("mmap succeeded for 1GB with 2MB page size\n");
     }
     munmap(ptr, 1024ll * 1024 * 1024);
 
     // mmap section fallback to regular page size
     ptr = mmap(NULL, 1024ll * 1024 * 1024, PROT_READ | PROT_WRITE, MAP_PRIVATE | MAP_ANONYMOUS, -1, 0); // 1GB
-    if (ptr == MAP_FAILED) {
+    if (ptr == MAP_FAILED)
+    {
         perror("mmap failed for 1GB with regular page size");
         return 1;
-    } else {
+    }
+    else
+    {
         printf("mmap succeeded for 1GB with regular page size\n");
     }
     munmap(ptr, 1024ll * 1024 * 1024);
 
     // mmap section fallback to regular page size
-    ptr = mmap(NULL, 4ul * 1024 * 1024 * 1024, PROT_READ | PROT_WRITE, MAP_PRIVATE | MAP_ANONYMOUS, -1, 0); // 1GB
-    if (ptr == MAP_FAILED) {
-        perror("mmap failed for 1GB with regular page size");
+    ptr = mmap(NULL, 4ul * 1024 * 1024 * 1024, PROT_READ | PROT_WRITE, MAP_PRIVATE | MAP_ANONYMOUS | MAP_HUGETLB | MAP_HUGE_2MB, -1, 0); // 4GB
+    if (ptr == MAP_FAILED)
+    {
+        perror("mmap failed for 4GB with 2MB page size");
         return 1;
-    } else {
-        printf("mmap succeeded for 1GB with regular page size\n");
+    }
+    else
+    {
+        printf("mmap succeeded for 4GB with 2MB page size\n");
     }
     munmap(ptr, 4ul * 1024 * 1024 * 1024);
     return 0;
